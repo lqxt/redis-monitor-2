39785bb84e824a3d26b39132c4301bfbfb5cc1a6
chengpan168@126.com <chengpan168@126.com>
2013-09-06 11:25:03 +0800

    update style index

---
+++ src/main/webapp/js/index/index.js
@@ -0,0 +1,139 @@
+$(document).ready(function() {
+		$('.dropdown-toggle').dropdown() ;
+		
+		 Highcharts.setOptions({
+	            global: {
+	                useUTC: false
+	            }
+	        });
+	    
+	        var chart;
+	        $('#container').highcharts({
+	            chart: {
+	                type: 'spline',
+	                animation: Highcharts.svg, // don't animate in old IE
+	                marginRight: 10,
+	                events: {
+	                    load: function() {
+	                        // set up the updating of the chart each second
+	                        var series = this.series[0];
+	                        setInterval(function() {
+	                            var x = (new Date()).getTime(), // current time
+	                                y = Math.random() * (1000-800) + 800;
+	                            series.addPoint([x, y], true, true );
+	                        }, 1000);
+	                    }
+	                }
+	            },
+	            title: {
+	                text: 'memery'
+	            },
+	            xAxis: {
+	                type: 'datetime',
+	                tickPixelInterval: 150
+	            },
+	            yAxis: {
+	                title: {
+	                    text: 'kb'
+	                },
+	                plotLines: [{
+	                    value: 0,
+	                    width: 1,
+	                    color: '#808080'
+	                }]
+	            },
+	            tooltip: {
+	                formatter: function() {
+	                        return '<b>'+ this.series.name +'</b><br/>'+
+	                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+	                        Highcharts.numberFormat(this.y, 2);
+	                }
+	            },
+	            legend: {
+	                enabled: false
+	            },
+	            exporting: {
+	                enabled: false
+	            },
+	            series: [{
+	                name: 'memery',
+	                data: (function() {
+	                    var data = [],
+	                        time = (new Date()).getTime(),
+	                        i;
+	    
+	                    for (i = -19; i <= 0; i++) {
+	                        data.push({
+	                            x: time + i * 1000,
+	                            y: Math.random() * (1000 - 800) + 800
+	                        });
+	                    }
+	                    return data;
+	                })()
+	            }]
+	        });
+	        $('#keysChart').highcharts({
+	        	chart: {
+	        		type: 'spline',
+	        		animation: Highcharts.svg, // don't animate in old IE
+	        		marginRight: 10,
+	        		events: {
+	        			load: function() {
+	        				// set up the updating of the chart each second
+	        				var series = this.series[0];
+	        				setInterval(function() {
+	        					var x = (new Date()).getTime(), // current time
+	        					y = Math.random() * (1000-800) + 800;
+	        					series.addPoint([x, y], true, true );
+	        				}, 1000);
+	        			}
+	        		}
+	        	},
+	        	title: {
+	        		text: 'memery'
+	        	},
+	        	xAxis: {
+	        		type: 'datetime',
+	        		tickPixelInterval: 150
+	        	},
+	        	yAxis: {
+	        		title: {
+	        			text: 'kb'
+	        		},
+	        		plotLines: [{
+	        			value: 0,
+	        			width: 1,
+	        			color: '#808080'
+	        		}]
+	        	},
+	        	tooltip: {
+	        		formatter: function() {
+	        			return '<b>'+ this.series.name +'</b><br/>'+
+	        			Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+	        			Highcharts.numberFormat(this.y, 2);
+	        		}
+	        	},
+	        	legend: {
+	        		enabled: false
+	        	},
+	        	exporting: {
+	        		enabled: false
+	        	},
+	        	series: [{
+	        		name: 'memery',
+	        		data: (function() {
+	        			var data = [],
+	        			time = (new Date()).getTime(),
+	        			i;
+	        			
+	        			for (i = -19; i <= 0; i++) {
+	        				data.push({
+	        					x: time + i * 1000,
+	        					y: Math.random() * (1000 - 800) + 800
+	        				});
+	        			}
+	        			return data;
+	        		})()
+	        	}]
+	        });
+	});
