31b319dca7a73dfa78e428e823154ba3f3f42460
chengpan168@126.com <chengpan168@126.com>
2013-09-09 14:00:40 +0800

    Signed-off-by: chengpan168@126.com <chengpan168@126.com>

---
+++ src/main/java/com/redis/monitor/web/controller/MonitorController.java
@@ -0,0 +1,54 @@
+package com.redis.monitor.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.redis.monitor.SocketMonitor;
+
+@Controller
+public class MonitorController extends BaseProfileController{
+
+	@RequestMapping("/monitor.htm")
+	public ModelAndView toMonitor(){
+		ModelAndView mv = new ModelAndView("monitor") ;
+		return mv ;
+	}
+	
+	@RequestMapping("/monitor/start.htm")
+	@ResponseBody
+	public Object start(){
+		Map<String , Object> res = new HashMap<String , Object>() ;
+		res.put("status", 0) ;
+		try{
+			new Thread(new Runnable() {
+				public void run() {
+					redisManager.startMonitor() ;
+				}
+			}).start();;
+		
+		} catch(Exception e ) {
+			res.put("status", 1) ;
+			res.put("message", "开始监控时发生错误") ;
+		}
+		return res ;
+	}
+	
+	@RequestMapping("/monitor/stop.htm")
+	@ResponseBody
+	public Object stop(){
+		Map<String , Object> res = new HashMap<String , Object>() ;
+		res.put("status", 0) ;
+		try{
+			SocketMonitor.disconnectClient();
+		} catch(Exception e ) {
+			res.put("status", 1) ;
+			res.put("message", "停止监控时发生错误") ;
+		}
+		return res ;
+	}
+}
